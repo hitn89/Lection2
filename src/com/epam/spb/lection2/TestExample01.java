9e035bce0279e60cc0e2a03db828798ae457632f
hitn <hitn89@gmail.com>
2016-06-28 18:15:17 +0300

    Test 01

---
+++ src/com/epam/spb/lection2/TestExample01.java
@@ -0,0 +1,34 @@
+package com.epam.spb.lection2;
+
+/**
+ * Created by hitn on 28.06.2016.
+ */
+public class TestExample01 {
+
+    public static void main(String[] args) {
+
+        byte b = 7;
+        System.out.println("byte = " + b);
+        short s = b;
+        System.out.println("short = " + s);
+        int i = s;
+        System.out.println("int = " + i);
+        char c = '3';
+        System.out.println("char = " + c);
+        int i2 = c;
+        System.out.println("int2 = " + i2);
+        float f = i2;
+        System.out.println("float = " + f);
+        long l = i2;
+        System.out.println("long = " + l);
+        double d = l;
+        System.out.println("double = " + d);
+        double d2 = f;
+        System.out.println("double2 = " + d2);
+        double d3 = i2;
+        System.out.println("double3 = " + d3);
+        float f2 = l;
+        System.out.println("float2 = " + f2);
+    }
+
+}
